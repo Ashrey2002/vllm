03fc9b351ee38306c7bb0984a41f8f4b8c3c41d8
Ashrey2002 <ashrey@petavue.com>
2024-06-05 10:59:45 +0530

    Update new_vllm.py

--- benchmark_scripts/new_vllm.py
+++ benchmark_scripts/new_vllm.py
@@ -161,6 +161,7 @@ def run_inferences() -> None:
             model=model_name,
             tensor_parallel_size=torch.cuda.device_count(),
             download_dir=SelfHosted.MODEL_WEIGHTS_DIRECTORY,
+            MAX_PARALLEL_LOADING_WORKERS=4,
         )
         
         sampling_params = SamplingParams(max_tokens=Defaults.MAX_TOKENS_TO_GENERATE)
